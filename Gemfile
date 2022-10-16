37052606c8f091ebc1765085b07574d07709b661
Edward Heaver <edward.heaver01@gmail.com>
2022-10-16 13:34:10 +0100

    added colours to test output

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'rake', '~> 13.0'
 
 gem 'minitest', '~> 5.0'
+gem 'minitest-reporters'
 
 gem 'rubocop', '~> 1.21'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,5 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'rb_cron_parser'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
