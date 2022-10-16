3440c7a85cff10cc16449c5780d44b97746da8fb
Edward Heaver <edward.heaver01@gmail.com>
2022-10-16 15:49:16 +0100

    fixed up cases

--- test/rb_cron_parser/parser/test_parser.rb
+++ test/rb_cron_parser/parser/test_parser.rb
@@ -17,53 +17,21 @@ module RbCronParser
               day_of_month: [3],
               month: [4],
               day_of_week: [5],
-              command: nil
+              command: nil,
             }
           },
           {
             scenario: 'Can parse wildcards',
             input: ['*', '*', '*', '*', '*'],
             expected: {
-              minute: [*0..59],
-              hour: [*0..23],
-              day_of_month: [*1..30],
-              month: [*1..12],
-              day_of_week: [*1..7]
+              minute: [*0..RbCronParser::IntervalTotal::MIN],
+              hour: [*0..RbCronParser::IntervalTotal::HOUR],
+              day_of_month: [*1..RbCronParser::IntervalTotal::DAY_OF_MONTH],
+              month: [*1..RbCronParser::IntervalTotal::MONTH],
+              day_of_week: [*1..RbCronParser::IntervalTotal::DAY_OF_WEEK],
+              command: nil,
             }
           },
-          {
-            scenario: 'Can parse ranges',
-            input: %w[1-5 1-5 0-5 0-5 0-5],
-            expected: {
-              day: [*1..5],
-              month: [*1..5],
-              hour: [*0..5],
-              minute: [*0..5],
-              second: [*0..5]
-            }
-          },
-          {
-            scenario: 'Can parse all intervals divisible by 5',
-            input: ['*/5', '*/5', '*/5', '*/5', '*/5'],
-            expected: {
-              day: [5],
-              month: [5, 10],
-              hour: [0, 5, 10, 15, 20],
-              minute: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55],
-              second: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
-            }
-          },
-          {
-            scenario: 'Can parse a complex expression',
-            input: ['1-5', '*', '*/2', '0', '0'],
-            expected: {
-              day: [*1..5],
-              month: [*1..12],
-              hour: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
-              minute: [0],
-              second: [0]
-            }
-          }
         ]
 
         tests.each do |test_case|
